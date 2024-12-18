a41f668b3be085639eca7a565d97bb0617ff8608
bellahejin <15931228548@163.com>
2024-12-18 18:40:10 +0800

    项目提交

---
+++ src/main/java/com/hejin/maven/SayHello.java
@@ -0,0 +1,11 @@
+package com.hejin.maven;
+
+/**
+ * @author hejin
+ * @create 2024-12-18-16:44
+ */
+public class SayHello {
+    public String say(String args) {
+        return "Hello " + args;
+    }
+}
---
+++ src/test/java/com/hejin/maven/HelloTest.java
@@ -0,0 +1,17 @@
+package com.hejin.maven;
+
+import org.junit.Test;
+
+/**
+ * @author hejin
+ * @create 2024-12-18-16:48
+ */
+public class HelloTest {
+
+    @Test
+    public  void  testHello() {
+        SayHello hello = new SayHello();
+        String maven = hello.say("maven");
+        System.out.println(maven);
+    }
+}
