a990810866f9b75ddba406aa25dabe33813496e9
bellahejin <15931228548@163.com>
2024-12-18 19:14:16 +0800

    deev

--- src/test/java/com/hejin/maven/HelloTest.java
+++ src/test/java/com/hejin/maven/HelloTest.java
@@ -11,7 +11,7 @@ public class HelloTest {
     @Test
     public  void  testHello() {
         SayHello hello = new SayHello();
-        String maven = hello.say("maven！！dev");
+        String maven = hello.say("maven！！deev");
         System.out.println(maven);
     }
 }
